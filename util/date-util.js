6053c33d442a3c94888b1569d33139031dbc51b8
hi-jin <crushed7@o.cnu.ac.kr>
2022-11-26 18:20:10 +0900

    add: date util function

---
+++ util/date-util.js
@@ -0,0 +1,17 @@
+/**
+ * 
+ * @param {Date} date 
+ */
+export function getDayOfWeekString(date) {
+    const dayOfWeek = ["일", "월", "화", "수", "목", "금", "토"];
+    return dayOfWeek[date.getDay()];
+}
+
+/**
+ * 
+ * @param {Date} date 
+ * @param {number} offset 
+ */
+export function getOffsetDate(date, offset) {
+    return new Date(date.getFullYear(), date.getMonth()+1, date.getDay()+offset);
+}
