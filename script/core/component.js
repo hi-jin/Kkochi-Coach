dc21fc8cc2254ce0b0e238f3ea14a7316b536bec
hi-jin <crushed7@o.cnu.ac.kr>
2022-11-26 14:07:03 +0900

    add: component class

---
+++ script/core/component.js
@@ -0,0 +1,20 @@
+export default class Component {
+    constructor() {
+        this.state = {};
+    }
+
+    /**
+     * Component가 사용하는 상태에 newState를 추가
+     * 
+     * @param {Map} newState 
+     */
+    setState(newState) {
+        this.state = { ...this.state, ...newState };
+        this.render();
+    }
+
+    /**
+     * this.state를 이용하여 화면에 출력
+     */
+    render() { }
+}
