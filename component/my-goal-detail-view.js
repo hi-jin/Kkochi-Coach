06376490410d4ed290655eb726fd45089dc77855
hi-jin <crushed7@o.cnu.ac.kr>
2022-12-04 21:11:59 +0900

    add: responsive UI

--- component/my-goal-detail-view.js
+++ component/my-goal-detail-view.js
@@ -3,8 +3,6 @@ import MyGoalData from "../data/my-goal-data.js";
 import Todo from "../domain/todo.js";
 import { dateToString } from "../util/date-util.js";
 
-const CANVAS_HEIGHT = 200;
-
 export default class MyGoalDetailView extends Component {
     /**
      * 
@@ -14,16 +12,10 @@ export default class MyGoalDetailView extends Component {
         super();
         this.html = myGoalDetailView;
         const myGoalData = new MyGoalData();
-        this.setState({
-            "myGoalData": myGoalData,
-            "innerWidth": innerWidth - 20,
-        });
+        this.setState({ "myGoalData": myGoalData });
         myGoalData.addListener(() => this.setState({
             "myGoalData": myGoalData,
         }));
-        window.addEventListener("resize", () => {
-            this.setState({ "innerWidth": innerWidth - 20 });
-        });
     }
 
     render() {
@@ -35,8 +27,6 @@ export default class MyGoalDetailView extends Component {
         this.html.appendChild(document.createTextNode("(클릭하여 자세히 확인해보세요!)"));
         this.html.appendChild(document.createElement("br"));
         this.html.appendChild(document.createElement("br"));
-        const timeline = this._timeline.call(this);
-        if (timeline !== null) this.html.appendChild(timeline);
         this.html.appendChild(this._todoListDiv.call(this));
     }
 
@@ -71,120 +61,6 @@ export default class MyGoalDetailView extends Component {
         return div;
     }
 
-    _timeline() {
-        const canvasWidth = this.state["innerWidth"];
-
-        const selectedTodo = this.myGoalData.selectedTodo;
-        if (selectedTodo === null) return null;
-
-        const canvas = document.createElement("canvas");
-        canvas.width = canvasWidth;
-        canvas.height = CANVAS_HEIGHT;
-        const ctx = canvas.getContext("2d");
-
-        const clearSet = this.clearSet;
-        let clearCount = 0;
-        let failCount = 0;
-        for (let date = selectedTodo.startDate; date <= new Date(); date = new Date(date.getFullYear(), date.getMonth(), date.getDate() + 1)) {
-            if (selectedTodo.repeatDayOfWeek.includes(date.getDay())) {
-                if (clearSet.has(dateToString(date))) {
-                    clearCount += 1;
-                } else {
-                    failCount += 1;
-                }
-            }
-        }
-        animateCircle(canvas, ctx, () => {
-            ctx.fillStyle = "black";
-            ctx.font = "18px Gowun Dodum";
-            ctx.fillText(`${selectedTodo.what}`, 210, 40);
-            ctx.fillStyle = "grey";
-            ctx.fillText(`총 ${clearCount + failCount}일 중 ${clearCount}일 성공`, 210, 70);
-            ctx.fillText(`${dateToString(selectedTodo.startDate)}부터`, 210, 120);
-            ctx.fillText(`${(selectedTodo.endDate === null) ? "진행중" : dateToString(selectedTodo.endDate) + "까지"}`, 210, 150);
-
-            setTimeout(() => {
-                scrollTo({
-                    top: document.body.scrollHeight,
-                    left: 0,
-                    behavior: "smooth",
-                });
-            }, 300);
-        });
-
-        return canvas;
-
-
-        /**
-         * 
-         * @param {HTMLCanvasElement} canvas 
-         * @param {CanvasRenderingContext2D} ctx 
-         * @param {Function} callback 
-         * @param {number} cur 
-         * @returns 
-         */
-        function animateCircle(canvas, ctx, callback, cur = 0) {
-            ctx.clearRect(0, 0, canvas.width, canvas.height);
-            drawCircleGraph(ctx, clearCount, failCount);
-
-            const x = 100;
-            const y = 100;
-            const radius = 90;
-
-            if (cur >= 2 * Math.PI) {
-                callback();
-                return;
-            }
-
-            ctx.moveTo(x, y);
-            ctx.beginPath();
-            ctx.fillStyle = "white";
-            ctx.arc(x, y, radius + 1, -0.5 * Math.PI + cur, 1.5 * Math.PI);
-            ctx.lineTo(x, y);
-            ctx.closePath();
-            ctx.fill();
-
-            setTimeout(() => {
-                animateCircle(canvas, ctx, callback, cur + 0.15);
-            }, 10);
-        }
-
-        /**
-         * 
-         * @param {CanvasRenderingContext2D} ctx 
-         * @param {number} data1 
-         * @param {number} data2 
-         */
-        function drawCircleGraph(ctx, data1, data2) {
-            const x = 100;
-            const y = 100;
-            const radius = 90;
-            const unit = (2 * Math.PI) / (data1 + data2);
-            ctx.moveTo(x, y);
-            ctx.beginPath();
-            ctx.fillStyle = "green";
-            ctx.arc(x, y, radius, -0.5 * Math.PI, unit * data1 - 0.5 * Math.PI);
-            ctx.lineTo(x, y);
-            ctx.closePath();
-            ctx.fill();
-
-            ctx.moveTo(x, y);
-            ctx.beginPath();
-            ctx.fillStyle = "red";
-            ctx.arc(x, y, radius, unit * data1 - 0.5 * Math.PI, 1.5 * Math.PI);
-            ctx.lineTo(x, y);
-            ctx.closePath();
-            ctx.fill();
-
-            ctx.moveTo(x, y);
-            ctx.beginPath();
-            ctx.fillStyle = "white";
-            ctx.arc(x, y, radius / 2, 0, 2 * Math.PI);
-            ctx.closePath();
-            ctx.fill();
-        }
-    }
-
     /**
      * @returns {Array<Todo>}
      */
@@ -192,20 +68,6 @@ export default class MyGoalDetailView extends Component {
         const selectedGoal = this.myGoalData.selectedGoal;
         const todoList = this.myGoalData.goalTodoMap.get(selectedGoal);
         return todoList ?? [];
-    }
-
-    get clearSet() {
-        const set = new Set();
-        for (const log of this.clearLog) {
-            set.add(dateToString(log.date));
-        }
-        return set;
-    }
-
-    get clearLog() {
-        const selectedTodo = this.myGoalData.selectedTodo;
-        if (selectedTodo === null) return [];
-        return this.myGoalData.clearLog.filter(log => log.todoId === selectedTodo.id);
     }
 
     /**
--- component/my-goal-todo-detail-view.js
+++ component/my-goal-todo-detail-view.js
@@ -5,12 +5,20 @@ import { addPost } from "../repo/post-repo.js";
 import { finishTodo, loadTodoList, removeTodo } from "../repo/todo-repo.js";
 import { dateToString } from "../util/date-util.js";
 
+const CANVAS_HEIGHT = 200;
+
 export default class MyGoalTodoDetailView extends Component {
     constructor(myGoalTodoDetailView) {
         super();
         this.html = myGoalTodoDetailView;
         const myGoalData = new MyGoalData();
-        this.setState({ "myGoalData": myGoalData });
+        this.setState({
+            "myGoalData": myGoalData,
+            "innerWidth": innerWidth - 40,
+        });
+        window.addEventListener("resize", () => {
+            this.setState({ "innerWidth": innerWidth - 40 });
+        });
         myGoalData.addListener(() => this.setState({ "myGoalData": myGoalData }));
     }
 
@@ -19,11 +27,138 @@ export default class MyGoalTodoDetailView extends Component {
 
         if (this.myGoalData.selectedTodo === null) return;
 
+        const timeline = this._timeline.call(this);
+        if (timeline !== null) this.html.appendChild(timeline);
         this.html.appendChild(this._whatDiv.call(this));
         this.html.appendChild(this._whereWhenDiv.call(this));
         this.html.appendChild(this._repeatDayOfWeekDiv.call(this));
         this.html.appendChild(this._descDiv.call(this));
         this.html.appendChild(this._buttonDiv.call(this));
+    }
+
+
+    _timeline() {
+        const canvasWidth = this.state["innerWidth"];
+
+        const selectedTodo = this.myGoalData.selectedTodo;
+        if (selectedTodo === null) return null;
+
+        const canvas = document.createElement("canvas");
+        canvas.width = canvasWidth;
+        canvas.height = CANVAS_HEIGHT;
+        const ctx = canvas.getContext("2d");
+
+        const clearSet = this.clearSet;
+        let clearCount = 0;
+        let failCount = 0;
+        for (let date = selectedTodo.startDate; date <= new Date(); date = new Date(date.getFullYear(), date.getMonth(), date.getDate() + 1)) {
+            if (selectedTodo.repeatDayOfWeek.includes(date.getDay())) {
+                if (clearSet.has(dateToString(date))) {
+                    clearCount += 1;
+                } else {
+                    failCount += 1;
+                }
+            }
+        }
+        animateCircle(canvas, ctx, () => {
+            ctx.fillStyle = "black";
+            ctx.font = "18px Gowun Dodum";
+            ctx.fillText(`${selectedTodo.what}`, 210, 40);
+            ctx.fillStyle = "black";
+            ctx.fillText(`총 ${clearCount + failCount}일 중 ${clearCount}일 성공`, 210, 70);
+            ctx.fillText(`${dateToString(selectedTodo.startDate)}부터`, 210, 120);
+            ctx.fillText(`${(selectedTodo.endDate === null) ? "진행중" : dateToString(selectedTodo.endDate) + "까지"}`, 210, 150);
+
+            setTimeout(() => {
+                scrollTo({
+                    top: document.body.scrollHeight,
+                    left: 0,
+                    behavior: "smooth",
+                });
+            }, 300);
+        });
+
+        return canvas;
+
+
+        /**
+         * 
+         * @param {HTMLCanvasElement} canvas 
+         * @param {CanvasRenderingContext2D} ctx 
+         * @param {Function} callback 
+         * @param {number} cur 
+         * @returns 
+         */
+        function animateCircle(canvas, ctx, callback, cur = 0) {
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+            drawCircleGraph(ctx, clearCount, failCount);
+
+            const x = 100;
+            const y = 100;
+            const radius = 90;
+
+            if (cur >= 2 * Math.PI) {
+                callback();
+                return;
+            }
+
+            ctx.moveTo(x, y);
+            ctx.beginPath();
+            ctx.fillStyle = "#ECECEC";
+            ctx.arc(x, y, radius + 1, -0.5 * Math.PI + cur, 1.5 * Math.PI);
+            ctx.lineTo(x, y);
+            ctx.closePath();
+            ctx.fill();
+
+            setTimeout(() => {
+                animateCircle(canvas, ctx, callback, cur + 0.15);
+            }, 10);
+        }
+
+        /**
+         * 
+         * @param {CanvasRenderingContext2D} ctx 
+         * @param {number} data1 
+         * @param {number} data2 
+         */
+        function drawCircleGraph(ctx, data1, data2) {
+            const x = 100;
+            const y = 100;
+            const radius = 90;
+            const unit = (2 * Math.PI) / (data1 + data2);
+            ctx.moveTo(x, y);
+            ctx.beginPath();
+            ctx.fillStyle = "green";
+            ctx.arc(x, y, radius, -0.5 * Math.PI, unit * data1 - 0.5 * Math.PI);
+            ctx.lineTo(x, y);
+            ctx.closePath();
+            ctx.fill();
+
+            ctx.moveTo(x, y);
+            ctx.beginPath();
+            ctx.fillStyle = "red";
+            ctx.arc(x, y, radius, unit * data1 - 0.5 * Math.PI, 1.5 * Math.PI);
+            ctx.lineTo(x, y);
+            ctx.closePath();
+            ctx.fill();
+
+            if (data1 === 0 && data2 === 0) {
+                ctx.moveTo(x, y);
+                ctx.beginPath();
+                ctx.fillStyle = "grey";
+                ctx.arc(x, y, radius, - 0.5 * Math.PI, 1.5 * Math.PI);
+                ctx.lineTo(x, y);
+                ctx.closePath();
+                ctx.fill();
+            }
+
+            ctx.moveTo(x, y);
+            ctx.beginPath();
+            ctx.fillStyle = "#ECECEC";
+            ctx.arc(x, y, radius / 2, 0, 2 * Math.PI);
+            ctx.closePath();
+            ctx.fill();
+        }
     }
 
     _whatDiv() {
@@ -221,5 +356,19 @@ export default class MyGoalTodoDetailView extends Component {
      */
     get myGoalData() {
         return this.state["myGoalData"];
+    }
+
+    get clearSet() {
+        const set = new Set();
+        for (const log of this.clearLog) {
+            set.add(dateToString(log.date));
+        }
+        return set;
+    }
+
+    get clearLog() {
+        const selectedTodo = this.myGoalData.selectedTodo;
+        if (selectedTodo === null) return [];
+        return this.myGoalData.clearLog.filter(log => log.todoId === selectedTodo.id);
     }
 }
